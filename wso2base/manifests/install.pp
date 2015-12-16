f56db190a650eaf370603e9599defe98637c629e
Akila Perera <ravihansa3000@gmail.com>
2015-12-16 15:22:32 +0530

    Set carbon_home permissions and ownership via exec resource type. This is to avoid performance issues and unnecessary logs caused by Puppet file resource.

--- wso2base/manifests/install.pp
+++ wso2base/manifests/install.pp
@@ -19,7 +19,7 @@ define wso2base::install ($mode, $install_dir, $pack_filename, $pack_dir, $owner
   $pack_file_abs_path = "${pack_dir}/${pack_filename}"
 
   # create directories for installation if they do not exist
-  ensure_resource('file', ["${install_dir}", "${pack_dir}"], { ensure => 'directory' })
+  ensure_resource('file', $pack_dir, { ensure => 'directory' })
 
   # download wso2 product pack archive
   case $mode {
@@ -27,47 +27,60 @@ define wso2base::install ($mode, $install_dir, $pack_filename, $pack_dir, $owner
       $remote_file_url = hiera("remote_file_url")
 
       ensure_resource('exec', "$pack_file_abs_path", {
-        path         => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-        cwd          => $pack_dir,
-        command      => "wget -q ${remote_file_url}",
-        logoutput    => 'on_failure',
-        creates      => $pack_file_abs_path,
-        onlyif       => "test -f $pack_file_abs_path",
-        timeout      => 0,
-        notify       => Exec["extract_${pack_file_abs_path}"],
-        require      => File["${install_dir}", "${pack_dir}"]
+        path           => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+        cwd            => $pack_dir,
+        command        => "wget -q ${remote_file_url}",
+        logoutput      => 'on_failure',
+        creates        => $pack_file_abs_path,
+        onlyif         => "test -f $pack_file_abs_path",
+        timeout        => 0,
+        notify         => Exec["extract_${pack_file_abs_path}"],
+        require        => File["${install_dir}", "${pack_dir}"]
       })
     }
 
     'file_bucket' : {
       ensure_resource('file', "$pack_file_abs_path", {
-        mode        => 750,
-        owner       => $owner,
-        group       => $group,
-        source      => ["puppet:///modules/${product_name}/${pack_filename}", "puppet:///packs/${pack_filename}"],
-        notify      => Exec["extract_${pack_file_abs_path}"],
-        require     => File["${install_dir}", "${pack_dir}"]
+        mode           => 750,
+        owner          => $owner,
+        group          => $group,
+        source         => ["puppet:///modules/${product_name}/${pack_filename}", "puppet:///files/packs/${pack_filename}"],
+        notify         => Exec["extract_${pack_file_abs_path}"],
+        require        => File["${install_dir}", "${pack_dir}"]
       })
     }
   }
 
   # extract downloaded wso2 product pack archive
   ensure_resource('exec', "extract_${pack_file_abs_path}", {
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    cwd         => $install_dir,
-    command     => "unzip ${pack_file_abs_path}",
-    logoutput   => 'on_failure',
-    creates     => "${carbon_home}/bin",
-    timeout     => 0,
-    refreshonly => true,
-    notify      => File["${carbon_home}"]
+    path               => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd                => $install_dir,
+    command            => "unzip ${pack_file_abs_path}",
+    logoutput          => 'on_failure',
+    creates            => "${carbon_home}/bin",
+    timeout            => 0,
+    refreshonly        => true,
+    notify             => Exec["set_ownership_${carbon_home}"]
   })
 
-  # set ownership and file permissions for carbon_home
-  ensure_resource('file', "${carbon_home}", {
-    ensure      => 'directory',
-    mode        => 754,
-    owner       => $owner,
-    group       => $group
+  # set ownership for carbon_home
+  ensure_resource('exec', "set_ownership_${carbon_home}", {
+    path               => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd                => $carbon_home,
+    command            => "chown -R ${owner}:${group} ./",
+    logoutput          => 'on_failure',
+    timeout            => 0,
+    refreshonly        => true,
+    notify             => Exec["set_permissions_${carbon_home}"]
+  })
+
+  # set file permissions for carbon_home
+  ensure_resource('exec', "set_permissions_${carbon_home}", {
+    path               => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    cwd                => $carbon_home,
+    command            => "chmod -R 754 ./",
+    logoutput          => 'on_failure',
+    timeout            => 0,
+    refreshonly        => true
   })
 }
