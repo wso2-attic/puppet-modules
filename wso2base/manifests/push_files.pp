473cb03f22ea34990e5845289ee2ebe049e79354
rajkumar <rraju1990@gmail.com>
2015-12-21 23:44:07 +0530

    fixing wso2base::push_files defined type

--- wso2base/manifests/push_files.pp
+++ wso2base/manifests/push_files.pp
@@ -14,13 +14,13 @@
 #  limitations under the License.
 #----------------------------------------------------------------------------
 
-define wso2base::push_files ($directory, $carbon_home, $owner, $group, $product_name, $product_version) {
+define wso2base::push_files ($carbon_home, $owner, $group, $product_name, $product_version) {
   file { "${carbon_home}/${name}":
     ensure  => present,
     owner   => $owner,
     group   => $group,
     recurse => remote,
     mode    => '0754',
-    source  => template("puppet:///modules/${product_name}/${product_version}/configs/${name}")
+    source  => ["puppet:///modules/${product_name}/configs/${name}"]
   }
 }
