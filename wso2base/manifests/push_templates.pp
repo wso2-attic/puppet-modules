2a8f0752ca7559afdb61a288f0106e949362381f
Akila Perera <ravihansa3000@gmail.com>
2015-12-23 15:02:00 +0530

    Use ensure_resource when declaring file resource type for deploying templates and files

--- wso2base/manifests/push_templates.pp
+++ wso2base/manifests/push_templates.pp
@@ -14,13 +14,12 @@
 #  limitations under the License.
 #----------------------------------------------------------------------------
 
-define wso2base::push_templates ($carbon_home, $owner, $group, $product_name, $product_version) {
-
-  file { "${carbon_home}/${name}":
+define wso2base::push_templates ($carbon_home, $owner, $group, $module_name) {
+  ensure_resource('file', "${carbon_home}/${name}", {
     ensure  => file,
     owner   => $owner,
     group   => $group,
     mode    => '0754',
-    content => template("${product_name}/${product_version}/${name}.erb")
-  }
+    content => template("${module_name}/${::product_version}/${name}.erb")
+  })
 }
