0e0320db753fc57ce1cd855465a093fc82a0ef83
anuruddhal <anuruddhal@wso2.com>
2016-07-20 17:53:16 +0530

    Adding check to copy cert file before importing

--- modules/wso2base/manifests/import_cert.pp
+++ modules/wso2base/manifests/import_cert.pp
@@ -32,6 +32,7 @@ define wso2base::import_cert ($carbon_home, $wso2_module, $java_home, $cert_file
       path      => "${java_home}/bin",
       cwd       => "${carbon_home}/repository/resources/security",
       command   => "keytool -importcert -noprompt -alias marathon-lb -keystore client-truststore.jks -storepass ${trust_store_password} -file ${cert_file}",
-      logoutput => 'on_failure'
+      logoutput => 'on_failure',
+      require  => File["${carbon_home}/repository/resources/security/${cert_file}"]
     }
   }
